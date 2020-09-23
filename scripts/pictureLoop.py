f5b6692b2e49d073d78bde8a3fbf04d26db59b8b
Daniel Nee <daniel.y.nee@gmail.com>
2020-09-23 13:35:37 -0400

    fixed conditional logic, removed magic numbers

--- scripts/pictureLoop.py
+++ scripts/pictureLoop.py
@@ -2,17 +2,25 @@ import os
 import time
 import datetime
 
-TIME_BETWEEN = 50  # Seconds
+# All time in seconds
+TIME_BETWEEN_PICTURES = 50
+TIME_BETWEEN_CLOSED_CHECK = 600
+
+# 24HR Time
+HOUR_CLOSE = 2
+HOUR_OPEN = 17
 
 nightClose = False
+
+TIME_BETWEEN = TIME_BETWEEN_PICTURES
 
 while True:
     currDatetime = datetime.datetime.now()
     now = currDatetime.timetuple()
     dateString = f'{now[0]}-{now[1]}-{now[2]}-{now[3]}:{now[4]:02}:{now[5]:02}'
     print(f'Current Time: {dateString}')
-    if now[3] < 2 or now[3] >= 17:
-        TIME_BETWEEN = 50
+    if now[3] < HOUR_CLOSE or now[3] >= HOUR_OPEN:
+        TIME_BETWEEN = TIME_BETWEEN_PICTURES
         nightClose = False
         try:
             os.system('rm ~/RedDoorLine/images/line/*')
@@ -22,8 +30,9 @@ while True:
         except Exception as e:
             print(f'Error: {e}')
             exit(1)
-    else if not nightClose:
+    elif not nightClose:
         os.system('aws s3 cp s3://www.reddoorline.com/images/status/closed.png s3://www.reddoorline.com/images/current.png')
-        TIME_BETWEEN = 600
+        TIME_BETWEEN = TIME_BETWEEN_CLOSED_CHECK
+        nightClose = True
     print(f'Pausing for {TIME_BETWEEN} seconds')
     time.sleep(TIME_BETWEEN)
