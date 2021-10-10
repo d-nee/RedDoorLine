9edea50750194b294306125eb6effe6bd3b55fe5
Daniel Nee <daniel.y.nee@gmail.com>
2021-10-09 20:07:59 -0700

    added test script

---
+++ scripts/test.py
@@ -0,0 +1,18 @@
+import os
+import time
+import datetime
+
+currDatetime = datetime.datetime.now()
+now = currDatetime.timetuple()
+dateString = f'{now[0]}-{now[1]}-{now[2]}-{now[3]}:{now[4]:02}:{now[5]:02}'
+print(f'Current Time: {dateString}')
+
+try:
+    os.system('rm ~/RedDoorLine/images/line/*')
+    os.system(f'raspistill -o ~/RedDoorLine/images/current.png' + \
+                f' -rot 90 -ex snow -h 808 -roi .5,.2,.333,1 -br 55')
+    os.system('aws s3 cp ~/RedDoorLine/images/current.png s3://www.reddoorline.com/images/current.png')
+except Exception as e:
+    print(f'Error: {e}')
+    exit(1)
+
