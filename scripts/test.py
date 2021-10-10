d971d34e57614e3890f36aa0cfbfa17ec76011bd
Daniel Nee <daniel.y.nee@gmail.com>
2021-10-09 20:50:23 -0700

    test directory separated

--- scripts/test.py
+++ scripts/test.py
@@ -9,7 +9,7 @@ print(f'Current Time: {dateString}')
 
 try:
     os.system('raspistill -o ~/RedDoorLine/images/current.png -rot 90 -ex snow -h 808 -roi .5,.2,.333,1 -br 55')
-    os.system(f'cp ~/RedDoorLine/images/current.png ~/RedDoorPics/{dateString}.png')
+    os.system(f'cp ~/RedDoorLine/images/current.png ~/RedDoorPics/test/{dateString}.png')
     os.system('aws s3 cp ~/RedDoorLine/images/current.png s3://www.reddoorline.com/images/current.png')
 except Exception as e:
     print(f'Error: {e}')
