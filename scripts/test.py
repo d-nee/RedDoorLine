1419887a1b8c50ce9e065e85e28d368bf208ecc1
Daniel Nee <daniel.y.nee@gmail.com>
2021-10-26 21:47:45 -0700

    fix test path

--- scripts/test.py
+++ scripts/test.py
@@ -15,7 +15,7 @@ try:
     os.system(f'raspistill -n -o {IMG_PATH} -vf -hf -ex snow -roi .25,.5,.7,.25')
     os.system(f'convert {IMG_PATH} -resize {RES*8//3}x{RES}! {IMG_PATH}')
     os.system(f'convert -pointsize {RES//15} -fill yellow -draw "text 0,{RES//15} \'{dateString}\'" {IMG_PATH} {IMG_PATH}')
-    os.system(f'cp {IMG_PATH} /media/pi/DNEE/{dateString}.png')
+    os.system(f'cp {IMG_PATH} /media/pi/DNEE/test/{dateString}.png')
     os.system(f'aws s3 cp {IMG_PATH} s3://www.reddoorline.com/images/current.png')
 except Exception as e:
     print(f'Error: {e}')
